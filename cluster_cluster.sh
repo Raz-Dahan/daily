49971ed922d25cf30b68f030f42addcd9d2a7c42
Raz-Dahan <razdahan31@gmail.com>
2023-07-23 19:56:05 +0300

    Add two clusters

---
+++ cluster_cluster.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+echo -e '\e[33mboker tov\e[0m, the subtle art of not giving a fuck starting'
+gcloud beta container --project "named-signal-392608" clusters create "test-cluster" \
+    --region "us-east1" \
+    --no-enable-basic-auth \
+    --cluster-version "1.26.5-gke.1200" \
+    --release-channel "regular" \
+    --machine-type "e2-medium" \
+    --image-type "COS_CONTAINERD" \
+    --disk-type "pd-balanced" \
+    --disk-size "32" \
+    --metadata disable-legacy-endpoints=true \
+    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+    --max-pods-per-node "110" \
+    --num-nodes "1" \
+    --logging=SYSTEM,WORKLOAD \
+    --monitoring=SYSTEM \
+    --enable-ip-alias \
+    --network "projects/named-signal-392608/global/networks/default" \
+    --subnetwork "projects/named-signal-392608/regions/us-east1/subnetworks/default" \
+    --no-enable-intra-node-visibility \
+    --default-max-pods-per-node "110" \
+    --enable-autoscaling \
+    --min-nodes "1" \
+    --max-nodes "2" \
+    --security-posture=standard \
+    --workload-vulnerability-scanning=disabled \
+    --no-enable-master-authorized-networks \
+    --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
+    --enable-autoupgrade \
+    --enable-autorepair \
+    --max-surge-upgrade 1 \
+    --max-unavailable-upgrade 0 \
+    --no-enable-managed-prometheus \
+    --enable-vertical-pod-autoscaling \
+    --enable-shielded-nodes \
+    --node-locations "us-east1-b" \
+    --location-policy "ANY"
+gcloud beta container --project "named-signal-392608" clusters create "prod-cluster" \
+    --zone "us-central1-a" \
+    --no-enable-basic-auth \
+    --cluster-version "1.26.5-gke.1200" \
+    --release-channel "regular" \
+    --machine-type "e2-medium" \
+    --image-type "COS_CONTAINERD" \
+    --disk-type "pd-balanced" \
+    --disk-size "32" \
+    --metadata disable-legacy-endpoints=true \
+    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+    --max-pods-per-node "110" \
+    --num-nodes "3" \
+    --logging=SYSTEM,WORKLOAD \
+    --monitoring=SYSTEM \
+    --enable-ip-alias \
+    --network "projects/named-signal-392608/global/networks/default" \
+    --subnetwork "projects/named-signal-392608/regions/us-central1/subnetworks/default" \
+    --no-enable-intra-node-visibility \
+    --default-max-pods-per-node "110" \
+    --enable-autoscaling \
+    --min-nodes "1" \
+    --max-nodes "5" \
+    --security-posture=standard \
+    --workload-vulnerability-scanning=disabled \
+    --no-enable-master-authorized-networks \
+    --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
+    --enable-autoupgrade \
+    --enable-autorepair \
+    --max-surge-upgrade 1 \
+    --max-unavailable-upgrade 0 \
+    --no-enable-managed-prometheus \
+    --enable-vertical-pod-autoscaling \
+    --enable-shielded-nodes \
+    --node-locations "us-central1-a" \
+    --location-policy "ANY"
+echo -e 'prod-cluster \e[35mcreated\e[0m'
+echo -e '\e[36mfinished\e[0m'
