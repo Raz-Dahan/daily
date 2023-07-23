e9ca579043ea551d109c8cd50c37b23d94bec35c
Raz-Dahan <razdahan31@gmail.com>
2023-07-23 19:56:37 +0300

    update zone

--- shutdowon_clusters.sh
+++ shutdowon_clusters.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcloud container clusters delete "prod-cluster" --project "named-signal-392608" --zone "us-central1-a" --quiet &
-gcloud container clusters delete "test-cluster" --project "named-signal-392608" --zone "us-east1-b" --quiet &
+gcloud container clusters delete "test-cluster" --project "named-signal-392608" --zone "us-east1" --quiet &
